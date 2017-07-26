9bafebfc9d59430313b516935a88ddcb2ef3b258
Glendale Acosta <gglendale17@gmail.com>
2017-07-26 10:40:48 -0700

    Added ratings and titles to movie card.

--- app-client/js/components/Gallery/MovieCard.js
+++ app-client/js/components/Gallery/MovieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import ReactStars from 'react-stars';
 
 const movieStyle = {
   width: '20%',
@@ -8,6 +9,11 @@ const movieStyle = {
   padding: '10px',
 };
 
+const titleStyle = {
+  color: '#fff',
+  fontSize: '13px',
+  marginBottom: '5px',
+};
 
 const MovieCard = (props) => {
   const innerMovieStyle = {
@@ -18,11 +24,22 @@ const MovieCard = (props) => {
     backgroundSize: 'cover',
     cursor: 'pointer',
   };
+
   return (
     <div style={movieStyle}>
       <Link to={`/movie/${props.id}`}>
         <div style={innerMovieStyle} />
       </Link>
+      <ReactStars
+        count={5}
+        size={20}
+        color2={'#ffd700'}
+        value={props.vote_average / 2}
+        edit={false}
+      />
+      <p style={titleStyle}>
+        {props.title} ({props.release_date.slice(0, 4)})
+      </p>
     </div>
   );
 };
@@ -30,7 +47,9 @@ const MovieCard = (props) => {
 MovieCard.propTypes = {
   poster_path: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
-  // title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  vote_average: PropTypes.number.isRequired,
+  release_date: PropTypes.string.isRequired,
 };
 
 export default MovieCard;
