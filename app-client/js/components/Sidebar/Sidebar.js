b709a436420229ee906ff19a3c26289e653182c4
Glendale Acosta <gglendale17@gmail.com>
2017-07-26 14:19:50 -0700

    Styled Sidebar.

--- app-client/js/components/Sidebar/Sidebar.js
+++ app-client/js/components/Sidebar/Sidebar.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Sidebar = () => {
-  return (
-    <div className="col-sm-2 sidebar">
-      <div className="list-group">
-        <button
-          type="button"
-          className="list-group-item list-group-item-action active dropdown-toggle"
-        >
-          Filters
-        </button>
-        <button type="button" className="list-group-item list-group-item-action">
-          Dapibus ac facilisis in
-        </button>
-        <button type="button" className="list-group-item list-group-item-action">
-          Morbi leo risus
-        </button>
-        <button type="button" className="list-group-item list-group-item-action">
-          Porta ac consectetur ac
-        </button>
-        <button type="button" className="list-group-item list-group-item-action" disabled>
-          Vestibulum at eros
-        </button>
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  showFilters = () => {
+    console.log('Hello');
+  }
+
+  showGenres = () => {
+    console.log('Hello');
+  }
+
+  render() {
+    return (
+      <div className="col-sm-2 sidebar">
+        <div className="list-group">
+          <button
+            type="button"
+            className="list-group-item list-group-item-action"
+          >
+            Favorites
+            <span className="dropdown-toggle" style={{marginLeft: 'auto'}} />
+          </button>
+          <button
+            type="button"
+            className="list-group-item list-group-item-action active"
+            onClick={this.showFilters}
+          >
+            Filters
+            <span className="dropdown-toggle" style={{marginLeft: 'auto'}} />
+          </button>
+          <button
+            type="button"
+            className="list-group-item list-group-item-action"
+            onClick={this.showGenres}
+          >
+            Genres
+            <span className="dropdown-toggle" style={{marginLeft: 'auto'}} />
+          </button>
+        </div>
+
+        <div className="list-group ">
+          <button
+            type="button"
+            className="list-group-item list-group-item-action"
+          >
+            Source Code
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Sidebar;
